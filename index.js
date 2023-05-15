1d638c689836bccef24b116009f71dae27886038
mjsri <107389044+mjsri@users.noreply.github.com>
2023-05-15 15:37:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World, Welcome to Jenkins trigger for github commit!')
+  response.send('Hello World, Welcome to Jenkins trigger for github commit')
 })
 
 app.listen(app.get('port'), function() {
